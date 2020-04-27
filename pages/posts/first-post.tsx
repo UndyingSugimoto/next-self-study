2b0f8e4770db976a796d138cd61ac8a8d3c1ea6f
UndyingSugimoto <kouji36000@yahoo.co.jp>
2020-04-28 01:55:58 +0900

    ブログの雛形作成まで完了

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -4,7 +4,7 @@ import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <Layout>
+    <Layout home={false}>
       <Head>
         <title>First Post</title>
       </Head>
